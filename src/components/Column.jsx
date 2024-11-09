9fa275df4ce09f2e13482796e28618f39a996f7b
Mohammad Afif <afifarfan808@gmail.com>
2024-11-09 12:20:42 +0600

    3rd

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,21 +1,21 @@
-import Card from './card';
+import Card from "./Card";
 
-const Column = ({title,number}) => {
+const Column = ({ title, number }) => {
   return (
     <div className='min-w-72 flex flex-col gap-4 bg-gray-200 rounded'>
-        <div className='flex justify-between text-[20px] px-5 py-1'>
+      <div className='flex justify-between text-[20px] px-5 py-1'>
         <span>{title}</span>
         <span>{number}</span>
-       </div> 
-       <div className='flex flex-col overflow-y-auto gap-4 p-2 rounded h-[80vh]'>
-        {Array.from({length:20}).map((_, index) => (
-          <Card key={index}/>
-        ))}
-       </div>
+      </div>
+      <div className='flex flex-col overflow-y-auto gap-4 p-2 rounded h-[80vh]'>
+        {Array.from({ length: 20 }).map(
+          (_, index) => (
+            <Card key={index} />
+          )
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Column;
-
-
