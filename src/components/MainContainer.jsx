8c302cb797c29945e826e4665f6723bb80e8632c
Mohammad Afif <afifarfan808@gmail.com>
2024-11-09 12:33:51 +0600

    packagejson

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,28 +1,30 @@
-import Column from './column';
+import Column from "./Column";
 
 const header = [
-  { title: "Incomplete" , number: 0},
-  { title: "To Do" , number: 10},
-  { title: "Doing" , number: 6},
-  { title: "Completed" , number: 11},
-  { title: "Under Review" , number: 8},
-  { title: "Over View" , number: 2},
-  { title: "Public view" , number: 16},
-  { title: "Only me" , number: 12},
-  { title: "Logistics" , number: 4},
-  { title: "Stats" , number: 9},
- ]
+  { title: "Incomplete", number: 0 },
+  { title: "To Do", number: 10 },
+  { title: "Doing", number: 6 },
+  { title: "Completed", number: 11 },
+  { title: "Under Review", number: 8 },
+  { title: "Over View", number: 2 },
+  { title: "Public view", number: 16 },
+  { title: "Only me", number: 12 },
+  { title: "Logistics", number: 4 },
+  { title: "Stats", number: 9 },
+];
 
 const MainContainer = () => {
   return (
     <div className='flex gap-4  bg-white h-screen p-4'>
-      {header.map(({title,number}) => (
-        <Column title={title} number={number} key={title}/>
+      {header.map(({ title, number }) => (
+        <Column
+          title={title}
+          number={number}
+          key={title}
+        />
       ))}
     </div>
-  )
-}
+  );
+};
 
 export default MainContainer;
-
-
